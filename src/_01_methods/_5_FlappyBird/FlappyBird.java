d570ba5601f3bb1d396e0ff13b55b754dac550e5
league <league@wts14-3.attlocal.net>
2024-01-06 11:29:03 -0800

    hjkrgfjxhjykhggajk

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -1,5 +1,7 @@
 package _01_methods._5_FlappyBird;
 
+import java.util.Random;
+
 import processing.core.PApplet;
 
 public class FlappyBird extends PApplet {
@@ -9,6 +11,13 @@ public class FlappyBird extends PApplet {
     int birdX=150;
     int grav = 1;
     int birdYVelocity=-5;
+    int pipeX =750;
+    Random ran = new Random();
+    int h1 = ran.nextInt(500);
+    int pipeGap = 150;
+    int lowerY = h1 +=pipeGap;
+    int upperY = 0;
+    
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
@@ -23,22 +32,29 @@ public class FlappyBird extends PApplet {
     @Override
     public void setup() {
     }
+   
     @Override
     public void draw() {
+    	background(255,255,255);
+    	if (pipeX < 1) {
+    		pipeX = 750;
+    		h1 = ran.nextInt(500);
+    	}
+	   
     	if (mousePressed) {
     		birdYVelocity=-10;
     	}
     	fill(0,255,0);
-    	rect(350,450,50,150);
+    	rect(pipeX,upperY,50,h1);
+    	rect(pipeX,lowerY,50,600);
     	birdYVelocity+=grav;
-    	background(255,255,255);
     	fill(255,255,0);
     	stroke(255,255,0);
     	ellipse(birdX, birdY, 50, 50);
     	birdY+=birdYVelocity;
+    	pipeX -= 8;
     }
-
-    static public void main(String[] args) {
+    static public void main(String[] args) { int pipeGap = 150;
         PApplet.main(FlappyBird.class.getName());
     }
 }
